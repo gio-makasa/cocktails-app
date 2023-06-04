79c902df2217281151834d149d96f611c4199638
gio <giorgimakasarashvili@gmail.com>
2023-06-04 11:22:50 +0400

    added alcoholic filter

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-list',
@@ -9,13 +9,24 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ListComponent {
   drinks: any = [];
+  filter: String = this.route.snapshot.params['filter'];
+  filterValue: String = this.route.snapshot.params['filterValue'];
 
   constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.http.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?${this.route.snapshot.params['filter']}=${this.route.snapshot.params['filterValue']}`).subscribe(
+
+    this.route.params.subscribe((params: Params) => {
+      this.filter = params['filter'];
+      this.filterValue = params['filterValue'];
+      this.getData();
+    })
+  }
+
+  getData() {
+    this.http.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?${this.filter}=${this.filterValue}`).subscribe(
       (data: any) => {
         if (data) {
           this.drinks = data.drinks;
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -10,7 +10,19 @@ import { Router } from '@angular/router';
 export class SearchComponent {
   searchWord: String = '';
   drinks: any = null;
+  alcoholics: { strAlcoholic: String }[] = [];
+
   constructor(private http: HttpClient, private router: Router) { }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+    this.http.get('https://www.thecocktaildb.com/api/json/v1/1/list.php?a=list').subscribe(
+      (data: any) => {
+        this.alcoholics = data.drinks
+      });
+  }
 
   Search(e: any) {
     if (e.value.length > 0) {
@@ -27,5 +39,9 @@ export class SearchComponent {
     this.drinks = null;
     this.searchWord = '';
     this.router.navigate(['/about', id]);
+  }
+
+  alcoholic(e: any) {
+    this.router.navigate(['/list', 'a', e.value]);
   }
 }
